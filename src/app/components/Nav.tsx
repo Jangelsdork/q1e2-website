e3f3d6a6e6e53a5414aa683947b64d28e5343a62
Jack Mangelsdorf <j.hespe.mangelsdorf@gmail.com>
2024-02-13 15:27:52 +0100

    converted svg to jsx component, changed colour of logo

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { panchang } from '../fonts'
 import Image from 'next/image'
 import Link from 'next/link'
+import Logo from '../components/Logo'
 
 type Props = {}
 
@@ -11,7 +12,10 @@ const Nav = (props: Props) => {
 
   return (
     <div className='w-full h-[25vh] border-red-500 border-4 fixed overflow-hidden top-0 grid grid-cols-3 bg-white'>
-    <Image src={"./Q1E2-RECORDS-LOGO.svg"} alt='Q1E2 tag logo' width={0} height={0} style={{width: 'auto', height: "20vh"}} className='self-center	 align-middle ml-3' ></Image>
+    {/* <Image src={"./Q1E2-RECORDS-LOGO.svg"} alt='Q1E2 tag logo' width={0} height={0} style={{width: 'auto', height: "20vh"}} className='self-center	 align-middle ml-3' ></Image> */}
+    <div className='self-center	 ml-3'>
+      <Logo />
+    </div>
     <div className={` text-2xl sm:text-6xl ${panchang.className} self-center  justify-self-center font-bold`}>Releases</div>
     <div className={`flex flex-col items-end mr-3 self-center text-xs sm:text-s ${panchang.className}`}>
         <Link href={"/"} className=' hover:font-bold hover":cursor-pointer '>Releases</Link>
