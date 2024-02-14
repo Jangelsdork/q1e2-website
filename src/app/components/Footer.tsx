46f495ffc6a002b1ec496aea22b5fbec9ec74f94
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-02-14 19:39:27 +0100

    footer additions

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,8 @@
-import { FaInstagram } from "react-icons/fa";
+/* eslint-disable arrow-body-style */
+import { FaInstagram, FaBandcamp } from "react-icons/fa";
+import { aoboshi, average } from '../fonts'
+import Link from "next/link";
+
 
 
 type Props = {}
@@ -6,9 +10,15 @@ type Props = {}
 const Footer = (props: Props) => {
   return (
     
-    <div className="w-full bg-white border-red-500 border-4 h-[20vh] flex flex-col items-end justify-center pr-8"> 
-    <FaInstagram size={50}  />
-
+    <div className="w-full bg-white border-red-500 border-4 h-[20vh] flex flex-row items-center justify-end pr-8"> 
+    <div className="flex flex-col items-end gap-2">
+      <div className="flex flex-row gap-4">
+      <Link href={"https://www.instagram.com/q1e2_recordings/"}><FaInstagram size={50}  /></Link>
+      <Link href={"https://q1e2recordings.bandcamp.com/"}><FaBandcamp size={50} /> </Link>
+      </div>
+      <div className={`${aoboshi.className} text-xs`}>© 2024 Q1E2 Recordings</div>
+      <div className={`${aoboshi.className} text-xs`}>Website design by <Link href={"https://jack-mangelsdorf.com/"}>Jack Mangelsdorf</Link></div>
+    </div>
     </div>
     
   )
