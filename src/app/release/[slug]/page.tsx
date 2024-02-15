8b4d18a51cf7f33e4613c94fc60b4a8ccb693246
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-02-15 21:28:47 +0100

    first draft of release page

--- src/app/release/[slug]/page.tsx
+++ src/app/release/[slug]/page.tsx
@@ -1,26 +1,35 @@
+/* eslint-disable import/extensions */
+
 import React from 'react'
 import { allReleases } from '@/app/components/Releases'
 import { aoboshi, average } from '../../fonts'
 import Image from 'next/image'
+import Link from 'next/link'
+import { FaInstagram } from "react-icons/fa";
+
+
 
 
 type Props = {}
 
 
 const page = ({ params }: { params: { slug: string } }) => {
-    const currentPageId = parseInt(params.slug, 10)
+    const currentPageId = parseInt(params.slug, 10) - 1 
 
   return (
     <div className='mt-[25vh] min-h-[55vh] sm:grid grid-cols-2 border-red-500 border-4'>
-        <div className='flex flex-col gap-8 m-8'>
+        <div className='flex flex-col gap-8 m-12'>
             <div className={`${aoboshi.className} text-8xl`}>{allReleases[currentPageId].artist}</div>        
             <div className={`${average.className} text-4xl`}>{allReleases[currentPageId].title}</div> 
             <div className={`${average.className} text-l`}>{allReleases[currentPageId].blurb}</div>
-            {allReleases[currentPageId].bandcamp}
-        </div> 
+            <div className='flex flex-row items-center justify-start gap-4'>
+                {allReleases[currentPageId].bandcamp}
+                <Link target="_blank" href={allReleases[currentPageId].instagram}><FaInstagram size={30} /> </Link>
+            </div>
+            </div> 
         <div className='flex justify-center items-center bg-red-500'>
             <div className='h-[50vh] w-[50vh] relative'>
-            <Image src={allReleases[currentPageId].artistImage} alt='picture of the artist' height={0} width={0} sizes='40vh' fill style={{objectFit:"cover", objectPosition:"top"}}></Image>
+            <Image src={allReleases[currentPageId].artistImage} alt='picture of the artist' height={0} width={0} sizes='40vh' fill style={{objectFit:"cover", objectPosition:"top"}} quality={100} unoptimized={true}></Image>
             </div>
         </div>      
     </div>
