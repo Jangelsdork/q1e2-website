53e136fbfc93c82900404d5290682c143a70af99
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-03-02 13:09:56 +0100

    stashed and reverted

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { aoboshi, average } from "../fonts";
+
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div className={`mt-[25vh] h-[55vh] bg-red-500 text-balance flex flex-col gap-4 justify-center items-center align-middle`}>
+        <div className={`${average.className} text-white text-center`}>Coming Soon...</div>
+        </div>
+  )
+}
+
+export default page
